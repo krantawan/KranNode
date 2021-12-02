5fa7f090dcd1b8943255fef66af6c88dcd8cf087
Krantawan <krantawan@ku.th>
2021-12-02 18:11:21 +0700

    commit test aws elastic beanstalk

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get("/", function(req,res){
     res.send("Hello! KRANTAWAN")
 })
 
+app.get("/hello", function(req,res){
+    res.send("Hello World!")
+})
+
 app.listen(process.env.PORT || 5000)
 module.exports = app
